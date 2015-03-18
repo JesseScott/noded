959aa4b5ac23a9cd67d23e585a559e2ad2d64fc6
JesseScott <jessecolinscott@gmail.com>
2015-03-18 08:53:33 -0700

    built android

--- noded/platforms/android/assets/www/js/app.js
+++ noded/platforms/android/assets/www/js/app.js
@@ -1,56 +1,50 @@
 (function(){
   'use strict';
-  var module = angular.module('app', ['onsen']);
 
-  module.controller('AppController', function($scope, $data) {
-    $scope.doSomething = function() {
-      setTimeout(function() {
-        alert('tappaed');
-      }, 100);
-    };
+  /* APP */
+
+  var module = angular.module('app', ['onsen', 'service']);
+
+  /* CONTROLLERS */
+
+  module.controller('AppController', function($scope) {
+
   });
 
-  module.controller('DetailController', function($scope, $data) {
+  var MasterController = function($scope, $data, ParseService) {
+        ParseService.getNodes(function(results) {
+          $scope.$apply(function() {
+            $scope.items = results;
+          });
+        });
+
+        $scope.showDetail = function(index) {
+          var item = $scope.items[index];
+          $data.selectedItem = item;
+          $scope.navigator.pushPage('detail.html', {title : item.get('ssid')});
+        };
+  }
+  MasterController.$inject = ['$scope', '$data', 'ParseService'];
+  module.controller('MasterController', MasterController);
+
+  module.controller('DetailController', function($scope, $data, ParseService) {
     $scope.item = $data.selectedItem;
-  });
 
-  module.controller('MasterController', function($scope, $data) {
-    $scope.items = $data.items;  
-    
-    $scope.showDetail = function(index) {
-      var selectedItem = $data.items[index];
-      $data.selectedItem = selectedItem;
-      $scope.navi.pushPage('detail.html', {title : selectedItem.title});
+    $scope.addNote = function(note) {
+        var obj = $scope.item;
+        ParseService.addNote(obj, note, function() {
+          //
+        });
     };
   });
+
+  /* FACTORIES */
 
   module.factory('$data', function() {
       var data = {};
-      
-      data.items = [
-          { 
-              title: 'Item 1 Title',
-              label: '4h',
-              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          },
-          { 
-              title: 'Another Item Title',
-              label: '6h',
-              desc: 'Ut enim ad minim veniam.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          }
-      ]; 
-      
       return data;
   });
-})();
 
+
+
+})();
---
+++ noded/platforms/android/assets/www/js/services.js
@@ -0,0 +1,126 @@
+'use strict';
+
+/* Services */
+
+angular.module('service', ['ngResource']).factory('ParseService', function($resource) {
+
+    // Initialize Parse API
+    Parse.initialize("BiYJKFD8IxfkHxzoTxfW4nYE3im1Jvhc6Jy2v7j8", "AtXojjwtcnc4a6WkxJZcTrq7smHEe4iRI2EKYVIw");
+
+    // Cache current logged in user
+    var loggedInUser;
+
+    // Define parse model and collection for Book records
+    var Node = Parse.Object.extend("Node");
+    var NodeCollection = Parse.Collection.extend({ model:Node });
+
+    var ParseService = {
+      name: "Parse",
+
+      // Login a user
+      login : function login(username, password, callback) {
+    	  Parse.User.logIn(username, password, {
+    	    success: function(user) {
+            loggedInUser = user;
+    	      callback(user);
+    	    },
+    	    error: function(user, error) {
+    	      alert("Error: " + error.message);
+    	    }
+        });
+      },
+
+      // Login a user using Facebook
+      FB_login : function FB_login(callback) {
+        Parse.FacebookUtils.logIn(null, {
+          success: function(user) {
+            if (!user.existed()) {
+              alert("User signed up and logged in through Facebook!");
+            } else {
+              alert("User logged in through Facebook!");
+            }
+            loggedInUser = user;
+            callback(user);
+          },
+          error: function(user, error) {
+            alert("User cancelled the Facebook login or did not fully authorize.");
+          }
+        });
+      },
+
+      // Register a user
+      signUp : function signUp(username, password, callback) {
+      	Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
+            success: function(user) {
+                loggedInUser = user;
+                callback(user);
+            },
+
+            error: function(user, error) {
+              alert("Error: " + error.message);
+            }
+        });
+      },
+
+      // Logout current user
+      logout : function logout(callback) {
+        Parse.User.logOut();
+      },
+
+      // Get All Nodes
+      getNodes : function getNodes(callback) {
+        var query = new Parse.Query(Node);
+        //query.include('notes');
+        query.find({
+          success : function(results) {
+            callback(results);
+          },
+          error : function(error) {
+            alert("Error" + error.message);
+          }
+        });
+      },
+
+      // Create A New Node
+      addNode : function addNode(_ssid, _password, _owner, _location, _mac, _image, callback) {
+        var object = new Node();
+        object.save({ssid:_ssid, password:_password, owner:_owner, point:_location, mac:_mac, thumbnail:_image}, {
+          success: function(object) {
+            callback();
+          },
+          error: function(error) {
+            alert("Error: " + error.message);
+          }
+        });
+      },
+
+      // Create A New Node
+      addNote : function addNote(_obj, _note) {
+        console.log('item is ' + _obj.id);
+        console.log('note is ' + _note);
+
+        var obj = new Node();
+        obj.id = _obj.id;
+        obj.addUnique('notes', _note);
+        obj.save(null, {
+          success: function(object) {
+            //callback(object);
+          },
+          error: function(error) {
+            alert("Error: " + error.message);
+          }
+        });
+      },
+
+      // Get current logged in user
+      getUser : function getUser() {
+        if(loggedInUser) {
+          return loggedInUser;
+        }
+      }
+
+    };
+
+    // The factory function returns ParseService, which is injected into controllers.
+    return ParseService;
+});
