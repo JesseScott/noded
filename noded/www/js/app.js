040e9d2ea159ec04ca3a40d58ad6c1bf5bae2fff
JesseScott <jessecolinscott@gmail.com>
2015-03-17 17:18:18 -0700

    passing data + styling

--- noded/www/js/app.js
+++ noded/www/js/app.js
@@ -18,37 +18,8 @@
   module.controller('DetailController', function($scope, $data) {
     $scope.item = $data.selectedItem;
   });
-/*
-  module.controller('MasterController', function($scope, $data, ParseService) {
 
-    // On startup...
-    $scope.nodeList = [];
-    //$scope.init();
-
-    // Fetch Nodes
-    $scope.getNodes = function() {
-      ParseService.getNodes(function(results) {
-        $scope.$apply(function() {
-          $scope.nodeList = results;
-          console.log("HERE");
-        });
-      });
-    }
-
-    $scope.items = $data.items;
-
-    $scope.showDetail = function(index) {
-      var selectedItem = $data.items[index];
-      $data.selectedItem = selectedItem;
-      $scope.navigator.pushPage('detail.html', {title : selectedItem.title});
-    };
-
-  });
-*/
   var MasterController = function($scope,  $data, ParseService) {
-        // On startup...
-        $scope.nodeList = [];
-        //$scope.init();
 
         // Fetch Nodes
           ParseService.getNodes(function(results) {
@@ -57,15 +28,13 @@
             });
           });
 
-        /*
-        $scope.items = $data.items;
-
         $scope.showDetail = function(index) {
-          var selectedItem = $data.items[index];
+          var selectedItem = $scope.items[index];
+          //alert(index);
           $data.selectedItem = selectedItem;
-          $scope.navigator.pushPage('detail.html', {title : selectedItem.title});
+          $scope.navigator.pushPage('detail.html', {title : selectedItem.get('ssid')});
         };
-        */
+
   }
   MasterController.$inject = ['$scope', '$data', 'ParseService'];
   module.controller('MasterController', MasterController);
@@ -101,51 +70,7 @@
 
       return data;
   });
-/*
-  module.factory('ParseService', function($resource) {
 
-      // Init
-      Parse.initialize("BiYJKFD8IxfkHxzoTxfW4nYE3im1Jvhc6Jy2v7j8", "AtXojjwtcnc4a6WkxJZcTrq7smHEe4iRI2EKYVIw");
-
-      // Define parse model and collection for Book records
-      var Node = Parse.Object.extend("Node");
-
-      var ParseService = {
-        name: "Parse",
-
-        // Get All Nodes
-        getNodes : function getNodes(callback) {
-          var query = new Parse.Query(Node);
-
-          query.find({
-            success : function(results) {
-              callback(results);
-            },
-            error : function(error) {
-              alert("Error" + error.message);
-            }
-          });
-        },
-
-        // Create A New Node
-        addNode : function addNode(_ssid, _password, _owner, _location, _mac, _image, callback) {
-          var object = new Node();
-          object.save({ssid:_ssid, password:_password, owner:_owner, point:_location, mac:_mac, thumbnail:_image}, {
-            success: function(object) {
-              callback();
-            },
-            error: function(error) {
-              alert("Error: " + error.message);
-            }
-          });
-        },
-
-      };
-
-      // The factory function returns ParseService, which is injected into controllers.
-      return ParseService;
-  });
-*/
 
 
 })();
