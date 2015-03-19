831020296310b8308784281af4075736ececcca9
JesseScott <jessecolinscott@gmail.com>
2015-03-19 13:51:32 -0700

    kind of broken phone gap stuff

--- noded/www/js/app.js
+++ noded/www/js/app.js
@@ -12,11 +12,24 @@
   });
 
   var MasterController = function($scope, $data, ParseService) {
-        ParseService.getNodes(function(results) {
-          $scope.$apply(function() {
-            $scope.items = results;
-          });
-        });
+        // ParseService.getNodes(function(results) {
+        //   $scope.$apply(function() {
+        //     $scope.items = results;
+        //   });
+        // });
+
+        document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady(){
+          alert("PhoneGap is ready.");
+        }
+
+        // geolocation.getCurrentPosition(function (position) {
+        //   alert('Latitude: '              + position.coords.latitude          + '\n' +
+        //         'Longitude: '             + position.coords.longitude         + '\n' +
+        //         'Altitude: '              + position.coords.altitude          + '\n' +
+        //         'Timestamp: '             + position.timestamp                + '\n'
+        //         );
+        // });
 
         $scope.showDetail = function(index) {
           var item = $scope.items[index];
@@ -44,6 +57,53 @@
       var data = {};
       return data;
   });
+
+
+  module.factory('geolocation', function ($rootScope, cordovaReady) {
+    return {
+        getCurrentPosition: cordovaReady(function (onSuccess, onError, options) {
+          navigator.geolocation.getCurrentPosition(function () {
+            var that = this,
+              args = arguments;
+
+            if (onSuccess) {
+              $rootScope.$apply(function () {
+                onSuccess.apply(that, args);
+              });
+            }
+          }, function () {
+            var that = this,
+              args = arguments;
+
+            if (onError) {
+              $rootScope.$apply(function () {
+                onError.apply(that, args);
+              });
+            }
+          },
+          options);
+        })
+      };
+  });
+
+  module.factory('cordovaReady', function() {
+    return function (fn) {
+      var queue = [];
+      var impl = function () {
+        queue.push(Array.prototype.slice.call(arguments));
+      };
+      document.addEventListener('deviceready', function () {
+        queue.forEach(function (args) {
+          fn.apply(this, args);
+        });
+        impl = fn;
+      }, false);
+      return function () {
+        return impl.apply(this, arguments);
+      };
+    };
+  });
+
 
 
 
